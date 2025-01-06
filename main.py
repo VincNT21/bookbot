251322ea169eb837b1af8ff5f46e2e5a63e9121b
VincNT_21 <vincnt_21@pm.me>
2025-01-06 11:47:51 +0100

    function ok!

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ def main():
     text = get_text(book_path)
     num_words = count_words(text)
     characters_dict = count_characters(text)
-    print(characters_dict)
+    list_dict = dict_to_list(characters_dict)
+    sorted_list = order_dict(list_dict)
+    alpha_list = filter_alpha(sorted_list)
+    make_report(book_path, num_words, alpha_list)    
 
 
 def get_text(path):
@@ -27,4 +30,34 @@ def count_characters(text):
     return char_dict
 
 
+def dict_to_list(dict):
+    list = []
+    for k in dict:
+        list.append({"char": k, "num": dict[k]})
+    return list
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
+def order_dict(list_dict):
+    list_dict.sort(reverse=True, key=sort_on)
+    return list_dict
+
+
+def filter_alpha(sorted_list):
+    return [item for item in sorted_list if item["char"].isalpha()]
+
+
+
+def make_report(book_path, num_words, ordered_dict):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print("")
+    for c in ordered_dict:
+        print(f"The '{c["char"]}' character was found {c["num"]} times")
+    print("--- End report---")
+
+     
 main()
